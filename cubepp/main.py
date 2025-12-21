74730effa27083597b086ef7a04c2e3a5d836699
Yuta Fujiyama <yuta.technology@gmail.com>
2025-12-22 00:04:23 +0900

    add _read

--- cubepp/main.py
+++ cubepp/main.py
@@ -129,6 +129,13 @@ endforeach()
   (void) file;
   HAL_UART_Transmit(&huart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
   return len;
+}
+
+int _read(int file, char *ptr, int len)
+{
+  (void) file;
+  HAL_UART_Receive(&huart4, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+	return len;
 }""",
             "check": "_write",
         },
